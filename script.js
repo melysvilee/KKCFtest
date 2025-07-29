2d6f93892e6608ae8ca6b295e0a0c8e56e5e7908
Mely <146790383+melysvilee@users.noreply.github.com>
2025-07-29 13:54:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function concat() {
+  var word1 = Number(document.getElementById("num1").value);
+  var word2 = Number(document.getElementById("num2").value);
+  var phraseStart = "There are many ";
+  var phraseStart2 = " ways to choose a";
+  var fullSentence = phraseStart + word1 + phraseStart2 + word2;
+  
+  document.getElementById("result").innerText = "Result: " + result;
+}
+}
